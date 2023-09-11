a33bf807d6d1a0c7747cb4ed95cb91cda70782e3
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-11 14:01:53 +0800

    refactor: use @vscode-use

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
 import * as vscode from 'vscode'
+import { CreateWebview } from '@vscode-use/createwebview'
+import { registerCommand } from '@vscode-use/utils'
 
-export function activate(context: any) {
+export function activate(context: vscode.ExtensionContext) {
   // todo: 解决iframe与vscode通信问题
-  vscode.commands.registerCommand('vscode-icones.openUrl', () => {
-    const panel = vscode.window.createWebviewPanel(
-      'webview panel',
-      'Icones',
-      vscode.ViewColumn.One,
-      {
-        enableScripts: true,
-        localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'media')],
+  const provider = new CreateWebview(
+    context.extensionUri,
+    {
+      title: 'Icones',
+      scripts: ['main.js'],
+    },
+  ) // css样式引入，本地css需要配置在media目录下
 
-      },
-    )
-    const scriptPathOnDisk = vscode.Uri.joinPath(context.extensionUri, 'media', 'main.js')
-
-    // And the uri we use to load this script in the webview
-    const scriptUri = panel.webview.asWebviewUri(scriptPathOnDisk)
-    const nonce = getNonce()
+  context.subscriptions.push(registerCommand('vscode-icones.openUrl', () => {
     const iconesUrl = 'https://vscode-icones.netlify.app/'
-    // dev url
-    // const iconesUrl = 'http://127.0.0.1:3333/'
-    panel.webview.html = `
+    provider.create(`
       <!DOCTYPE html>
       <html lang="en">
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>icones</title>
+          <title>Icones</title>
           <style>
             body{
               width:100%;
@@ -37,31 +30,18 @@ export function activate(context: any) {
         </head>
         <body>
           <iframe id="icones" src="${iconesUrl}" width="100%" height="100%"></iframe>
-          <script nonce="${nonce}" src="${scriptUri}"></script>
         </body>
       </html>
-    `
-    panel.webview.onDidReceiveMessage(
-      ({ type, data }) => {
-        // 设置剪贴板内容
-        if (type === 'copy') {
-          vscode.env.clipboard.writeText(data).then(() => {
-            vscode.window.showInformationMessage('复制成功!  ✅')
-          })
-        }
-      },
-    )
-  })
+      `, ({ data, type }) => {
+      // callback 获取js层的postMessage数据
+      if (type === 'copy') {
+        vscode.env.clipboard.writeText(data).then(() => {
+          vscode.window.showInformationMessage('复制成功!  ✅')
+        })
+      }
+    })
+  }))
 }
 
 export function deactivate() {
-}
-
-function getNonce() {
-  let text = ''
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for (let i = 0; i < 32; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-
-  return text
 }
